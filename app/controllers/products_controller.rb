06dc46c0761c71d878ab7551defddc48e118aa7f
Bernice Anne W. Chua & Lindsey Stevenson <pair+BerniceChua.lindsey-s@devbootcamp.com>
2016-02-09 10:31:37 -0800

    add views and controller methods to edit product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,38 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      p product_params
+      p params
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:title, :description, :price, :inventory)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'products/index'
 
+  resources :products
+
   root "products#index"
 end
