6574c4ce59a6840539103080493e91ea049621f0
Bernice Anne W. Chua & Lindsey Stevenson <pair+BerniceChua.lindsey-s@devbootcamp.com>
2016-02-09 15:13:42 -0800

    add validations to Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+  before_save :nil_inventory_is_zero
+
+  validates :title, :description, :price, presence: true
+
+  def nil_inventory_is_zero
+    if self.inventory.nil?
+      self.inventory = 0
+    end
+  end
+
 end
